1b450957d2999f042a86bc2a705b552940c25759
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-29 19:26:53 +0300

    fix top

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import "./App.css";
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
-import { Footer, Header, Hero, Top } from "./widgets";
+import { Header, Top } from "./widgets";
 
 function App() {
   return (
     <MantineProvider>
       <Top />
       <Header />
-      <Hero />
-      <Footer />
+      {/* <Hero /> */}
+      {/* <Footer /> */}
     </MantineProvider>
   );
 }
